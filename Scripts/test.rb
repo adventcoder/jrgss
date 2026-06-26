fe3deb4d02f2c3eec23fa5c758a9c221482cbc4a
adventcoder <59347361+adventcoder@users.noreply.github.com>
2026-06-26 05:55:56 +0100

    linux stuff

--- Scripts/test.rb
+++ Scripts/test.rb
@@ -1,21 +1,32 @@
 
-img = Bitmap.new('test/rat')
+img = Bitmap.new('Graphics/Battlers/Rat')
 img.save('test/rat.gif')
 
-b = Bitmap.new(100, 100)
+b = Bitmap.new(544, 416)
 
 b.set_pixel(0, 0, Color.new(5, 105, 205, 200))
 p(b.get_pixel(0, 0))
 
+white = Color.new(255, 255, 255)
 black = Color.new(0, 0, 0)
 red = Color.new(255, 0, 0)
 blue = Color.new(0, 0, 255)
 yellow = Color.new(255, 255, 0)
 
-b.fill_rect(b.rect, black)
-b.gradient_fill_rect(2, 2, 96, 96, blue, yellow, true)
-b.stretch_blt(Rect.new(10, 10, 80, 80), img, img.rect)
+b.fill_rect(b.rect, white)
 
-b.draw_text(2, 2, 96, 96, "Hello", 1)
+rect = Rect.new(0, 0, 100, 100)
+rect.x = (b.width - rect.width) / 2
+rect.y = (b.height - rect.height) / 2
+
+inner_rect = Rect.new(rect.x + 2, rect.y + 2, rect.width - 4, rect.height - 4)
+inner_rect2 = Rect.new(rect.x + 10, rect.y + 10, rect.width - 20, rect.height - 20)
+
+b.fill_rect(rect, black)
+b.gradient_fill_rect(inner_rect, blue, yellow, true)
+b.stretch_blt(inner_rect2, img, img.rect)
+
+b.draw_text(inner_rect, "Hello", 1)
+#b.hue_change(120)
 
 b.save('test/a.png')
